3789600becf0efeeb9ba851f860890390bfaf15e
Sebastiaan Mannem <sebas@mannem.nl>
2026-01-24 08:49:06 +0100

    Small fixes

--- internal/util/k8s_test.go
+++ internal/util/k8s_test.go
@@ -11,12 +11,14 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package util
 
 import (
+	"os"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"os"
 )
 
 var _ = Describe("k8s", func() {
@@ -89,7 +91,6 @@ users:
 			Expect(err).NotTo(HaveOccurred())
 			err = kubeconfigFile.Close()
 			Expect(err).NotTo(HaveOccurred())
-
 
 			kubeconfigPath := kubeconfigFile.Name()
 			context := "context2"
--- internal/util/user_test.go
+++ internal/util/user_test.go
@@ -16,10 +16,11 @@ package util
 
 import (
 	"errors"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 	"os"
 	"os/user"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("User", func() {
