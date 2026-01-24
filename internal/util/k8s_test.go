ef74349bd6d7a9bff94c888d923a1590703dbcc5
Sebastiaan Mannem <sebas@mannem.nl>
2026-01-24 07:56:15 +0100

    Adding unittests util

---
+++ internal/util/k8s_test.go
@@ -0,0 +1,111 @@
+// Copyright 2018 Sorint.lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package util
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"os"
+)
+
+var _ = Describe("k8s", func() {
+	Describe("PodName", func() {
+		var originalPodName string
+
+		BeforeEach(func() {
+			originalPodName = os.Getenv(KubePodName)
+		})
+
+		AfterEach(func() {
+			os.Setenv(KubePodName, originalPodName)
+		})
+
+		Context("when the POD_NAME environment variable is set", func() {
+			It("should return the pod name", func() {
+				expectedPodName := "my-pod"
+				os.Setenv(KubePodName, expectedPodName)
+				podName, err := PodName()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(podName).To(Equal(expectedPodName))
+			})
+		})
+
+		Context("when the POD_NAME environment variable is not set", func() {
+			It("should return an error", func() {
+				os.Unsetenv(KubePodName)
+				_, err := PodName()
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("NewKubeClientConfig", func() {
+		It("should create a client config with the provided parameters", func() {
+			kubeconfigContent := `
+apiVersion: v1
+clusters:
+- cluster:
+    server: https://server1:8443
+  name: cluster1
+- cluster:
+    server: https://server2:8443
+  name: cluster2
+contexts:
+- context:
+    cluster: cluster1
+    user: user1
+  name: context1
+- context:
+    cluster: cluster2
+    user: user2
+  name: context2
+current-context: context1
+kind: Config
+preferences: {}
+users:
+- name: user1
+  user:
+    token: token1
+- name: user2
+  user:
+    token: token2
+`
+			kubeconfigFile, err := os.CreateTemp("", "kubeconfig")
+			Expect(err).NotTo(HaveOccurred())
+			defer os.Remove(kubeconfigFile.Name())
+
+			_, err = kubeconfigFile.Write([]byte(kubeconfigContent))
+			Expect(err).NotTo(HaveOccurred())
+			err = kubeconfigFile.Close()
+			Expect(err).NotTo(HaveOccurred())
+
+
+			kubeconfigPath := kubeconfigFile.Name()
+			context := "context2"
+			namespace := "my-namespace"
+
+			clientConfig := NewKubeClientConfig(kubeconfigPath, context, namespace)
+
+			// verify namespace
+			ns, _, err := clientConfig.Namespace()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(ns).To(Equal(namespace))
+
+			// verify that the correct context is used
+			restConfig, err := clientConfig.ClientConfig()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(restConfig.Host).To(Equal("https://server2:8443"))
+		})
+	})
+})
--- internal/util/slice_test.go
+++ internal/util/slice_test.go
@@ -8,92 +8,91 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package util
 
-import "testing"
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
 
-func TestCompareStringSlice(t *testing.T) {
-	tests := []struct {
-		a  []string
-		b  []string
-		ok bool
-	}{
-		{[]string{}, []string{}, true},
-		{[]string{"", ""}, []string{""}, false},
-		{[]string{"", ""}, []string{"", ""}, true},
-		{[]string{"a", "b"}, []string{"a", "b"}, true},
-		{[]string{"a", "b"}, []string{"b", "a"}, false},
-		{[]string{"a", "b", "c"}, []string{"a", "b"}, false},
-		{[]string{"a", "b", "c"}, []string{"a", "b", "c"}, true},
-		{[]string{"a", "b", "c"}, []string{"b", "c", "a"}, false},
-		{[]string{"a", "b", "c", "a"}, []string{"a", "c", "b", "b"}, false},
-		{[]string{"a", "b", "c", "a"}, []string{"a", "c", "b", "b"}, false},
-	}
+var _ = Describe("Slice", func() {
+	Describe("CompareStringSlice", func() {
+		It("should correctly compare string slices", func() {
+			tests := []struct {
+				a  []string
+				b  []string
+				ok bool
+			}{
+				{[]string{}, []string{}, true},
+				{[]string{"", ""}, []string{""}, false},
+				{[]string{"", ""}, []string{"", ""}, true},
+				{[]string{"a", "b"}, []string{"a", "b"}, true},
+				{[]string{"a", "b"}, []string{"b", "a"}, false},
+				{[]string{"a", "b", "c"}, []string{"a", "b"}, false},
+				{[]string{"a", "b", "c"}, []string{"a", "b", "c"}, true},
+				{[]string{"a", "b", "c"}, []string{"b", "c", "a"}, false},
+				{[]string{"a", "b", "c", "a"}, []string{"a", "c", "b", "b"}, false},
+			}
 
-	for i, tt := range tests {
-		ok := CompareStringSlice(tt.a, tt.b)
-		if ok != tt.ok {
-			t.Errorf("%d: got %t but wanted: %t a: %v, b: %v", i, ok, tt.ok, tt.a, tt.b)
-		}
-	}
-}
+			for _, tt := range tests {
+				Expect(CompareStringSlice(tt.a, tt.b)).To(Equal(tt.ok))
+			}
+		})
+	})
 
-func TestCompareStringSliceNoOrder(t *testing.T) {
-	tests := []struct {
-		a  []string
-		b  []string
-		ok bool
-	}{
-		{[]string{}, []string{}, true},
-		{[]string{"", ""}, []string{""}, false},
-		{[]string{"", ""}, []string{"", ""}, true},
-		{[]string{"a", "b"}, []string{"a", "b"}, true},
-		{[]string{"a", "b"}, []string{"b", "a"}, true},
-		{[]string{"a", "b", "c"}, []string{"a", "b"}, false},
-		{[]string{"a", "b", "c"}, []string{"a", "b", "c"}, true},
-		{[]string{"a", "b", "c"}, []string{"b", "c", "a"}, true},
-		{[]string{"a", "b", "c", "a"}, []string{"a", "c", "b", "b"}, false},
-		{[]string{"a", "b", "c", "a"}, []string{"a", "c", "b", "b"}, false},
-	}
+	Describe("CompareStringSliceNoOrder", func() {
+		It("should correctly compare string slices ignoring order", func() {
+			tests := []struct {
+				a  []string
+				b  []string
+				ok bool
+			}{
+				{[]string{}, []string{}, true},
+				{[]string{"", ""}, []string{""}, false},
+				{[]string{"", ""}, []string{"", ""}, true},
+				{[]string{"a", "b"}, []string{"a", "b"}, true},
+				{[]string{"a", "b"}, []string{"b", "a"}, true},
+				{[]string{"a", "b", "c"}, []string{"a", "b"}, false},
+				{[]string{"a", "b", "c"}, []string{"a", "b", "c"}, true},
+				{[]string{"a", "b", "c"}, []string{"b", "c", "a"}, true},
+				{[]string{"a", "b", "c", "a"}, []string{"a", "c", "b", "b"}, false},
+			}
 
-	for i, tt := range tests {
-		ok := CompareStringSliceNoOrder(tt.a, tt.b)
-		if ok != tt.ok {
-			t.Errorf("%d: got %t but wanted: %t a: %v, b: %v", i, ok, tt.ok, tt.a, tt.b)
-		}
-	}
-}
+			for _, tt := range tests {
+				Expect(CompareStringSliceNoOrder(tt.a, tt.b)).To(Equal(tt.ok))
+			}
+		})
+	})
 
-func TestDifference(t *testing.T) {
-	tests := []struct {
-		a []string
-		b []string
-		r []string
-	}{
-		{[]string{}, []string{}, []string{}},
-		{[]string{"", ""}, []string{""}, []string{}},
-		{[]string{"", ""}, []string{"", ""}, []string{}},
-		{[]string{"", ""}, []string{"a", "", "b"}, []string{}},
-		{[]string{"a", "b"}, []string{"a", "b"}, []string{}},
-		{[]string{"a", "b"}, []string{"b", "a"}, []string{}},
-		{[]string{"a", "b", "c"}, []string{}, []string{"a", "b", "c"}},
-		{[]string{"a", "b", "c"}, []string{"a", "b"}, []string{"c"}},
-		{[]string{"a", "b"}, []string{"a", "b", "c"}, []string{}},
-		{[]string{"a", "b"}, []string{"c", "a", "b"}, []string{}},
-		{[]string{"a", "b", "c"}, []string{"a", "b", "c"}, []string{}},
-		{[]string{"a", "b", "c"}, []string{"b", "c", "a"}, []string{}},
-		{[]string{"a", "b", "c", "a"}, []string{"a", "c", "b", "b"}, []string{}},
-		{[]string{"a", "b", "c", "a"}, []string{"a", "c", "b", "b"}, []string{}},
-	}
+	Describe("Difference", func() {
+		It("should return the difference between two string slices", func() {
+			tests := []struct {
+				a []string
+				b []string
+				r []string
+			}{
+				{[]string{}, []string{}, []string{}},
+				{[]string{"", ""}, []string{""}, []string{}},
+				{[]string{"", ""}, []string{"", ""}, []string{}},
+				{[]string{"", ""}, []string{"a", "", "b"}, []string{}},
+				{[]string{"a", "b"}, []string{"a", "b"}, []string{}},
+				{[]string{"a", "b"}, []string{"b", "a"}, []string{}},
+				{[]string{"a", "b", "c"}, []string{}, []string{"a", "b", "c"}},
+				{[]string{"a", "b", "c"}, []string{"a", "b"}, []string{"c"}},
+				{[]string{"a", "b"}, []string{"a", "b", "c"}, []string{}},
+				{[]string{"a", "b"}, []string{"c", "a", "b"}, []string{}},
+				{[]string{"a", "b", "c"}, []string{"a", "b", "c"}, []string{}},
+				{[]string{"a", "b", "c"}, []string{"b", "c", "a"}, []string{}},
+				{[]string{"a", "b", "c", "a"}, []string{"a", "c", "b", "b"}, []string{}},
+			}
 
-	for i, tt := range tests {
-		r := Difference(tt.a, tt.b)
-		if !CompareStringSliceNoOrder(r, tt.r) {
-			t.Errorf("%d: got %v but wanted: %v a: %v, b: %v", i, r, tt.r, tt.a, tt.b)
-		}
-	}
-}
+			for _, tt := range tests {
+				Expect(Difference(tt.a, tt.b)).To(ConsistOf(tt.r))
+			}
+		})
+	})
+})
